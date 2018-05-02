737689ac975f80dc470ddbc2fd61ee608620f1ef
David <waywardblues@gmail.com>
2018-05-02 16:57:52 +0700

    Added SCSS styling

--- app/app.jsx
+++ app/app.jsx
@@ -10,7 +10,8 @@ var Examples = require('Examples');
 require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
 
-require('style!css!applicationStyles')
+// app css
+require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
   <Router history={hashHistory}>
